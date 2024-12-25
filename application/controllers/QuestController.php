58721b4b5f23ef1418bfb48283a96779dc8c0b9b
Rizkan00 <rizkan0990@gmail.com>
2024-12-26 05:06:53 +0700

    CRUD Quesioner

--- application/controllers/QuestController.php
+++ application/controllers/QuestController.php
@@ -23,9 +23,8 @@ class QuestController extends CI_Controller {
             $row[] = $result->quest;
             $row[] = $result->tgl_dibuat;
             $row[] = " <div class='d-flex'>
-                        <button type='button' onclick='lihatSertifikat($result->id_quest, 2)' class='btn btn-secondary m-1'><i class='fa-solid fa-eye'></i></button>
-                        <button type='button' onclick='lihatSertifikat($result->id_quest, 3)' class='btn btn-success m-1'><i class='fa-solid fa-pen-to-square'></i></button>
-                        <button type='button' onclick='lihatSertifikat($result->id_quest, 3)' class='btn btn-danger m-1'><i class='fa-solid fa-trash-can'></i></button>
+                        <button type='button' onclick='getDataQuest($result->id_quest)' class='btn btn-success m-1'><i class='fa-solid fa-pen-to-square'></i></button>
+                        <button type='button' onclick='deleteQuest($result->id_quest)' class='btn btn-danger m-1'><i class='fa-solid fa-trash-can'></i></button>
                     ";
 
             $data[] = $row;
@@ -49,8 +48,9 @@ class QuestController extends CI_Controller {
             $ganda = "";
             $text_bebas = 0;
 
+            
             $arr_pl = array();
-
+            
             if($this->input->post('jenis_jawaban') == 1 ){
                 
                 for($i = 1; $i <= 6; $i++){
@@ -60,21 +60,25 @@ class QuestController extends CI_Controller {
             }else{
                 $jenis_jawaban = 2;
             }
-
+            
             if($jenis_jawaban == 2){
-                $text_bebas = 0;
+                $text_bebas = 1;
             }
-
+            
             $formatted_date = date('Y-m-d H:i:s', time()); // Formatkan timestamp ke 'Y-m-d H:i:s'
-        
+            
             $data = array(
                 'quest' => $this->input->post('quest'),
                 'text_bebas' => $text_bebas,
                 'ganda' => json_encode($arr_pl),
                 'lainnya' => $this->input->post('lainnya_pil'),
-                'tgl_dibuat' => $formatted_date
+                'tgl_dibuat' => $formatted_date,
+                'updated' => $formatted_date
             );
-    
+            
+            // var_dump($data);
+            // die();
+
             $query = $this->QuestModels->InsertData('quesioner', $data);
     
             if($query){
@@ -84,9 +88,97 @@ class QuestController extends CI_Controller {
                 $this->session->set_Flashdata('error', 'Gagal Membuat Surat Balasan');
                 redirect('Administrator');
             }
-
-            var_dump($data);
-            die();
     
     }
+
+    public function getEditQuest(){
+        
+        $idQuest = $this->input->post('id');
+        $this->db->select("*");
+        $this->db->from('quesioner');
+        $this->db->where('id_quest', $idQuest);
+        $query = $this->db->get()->row_array();
+
+
+
+        $response = array(
+            'status' => 200,
+            'getDataQuest' => $query,
+        );
+
+        
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($response));
+    }
+
+    
+    public function Update(){
+        
+        $jenis_jawaban = 0;
+        $ganda = "";
+        $text_bebas = 0;
+
+        
+        $arr_pl = array();
+        
+        if($this->input->post('jenis_jawaban') == 1 ){
+            
+            for($i = 1; $i <= 6; $i++){
+                array_push($arr_pl, $this->input->post('pl'.$i));
+            }
+            $jenis_jawaban = 1;
+        }else{
+            $jenis_jawaban = 2;
+        }
+        
+        if($jenis_jawaban == 2){
+            $text_bebas = 1;
+        }
+
+        $formatted_date = date('Y-m-d H:i:s', time());
+
+        $data = array(
+            'quest' => $this->input->post('quest'),
+            'text_bebas' => $text_bebas,
+            'ganda' => json_encode($arr_pl),
+            'lainnya' => $this->input->post('lainnya_pil'),
+            'updated' => $formatted_date
+        );
+
+        $id_quest = $this->input->post('id_quest');
+        // var_dump($idPenilaian);
+        // die();
+        $this->db->where('id_quest', $this->input->post('id_quest'));
+
+        $query = $this->db->update('quesioner', $data);      
+
+        
+        if($query){
+            $this->session->set_Flashdata('success', 'suratSuccess');
+            redirect('Administrator');
+        }else{
+            $this->session->set_Flashdata('error', 'Gagal Membuat Surat Balasan');
+            redirect('Administrator');
+        }
+
+    }
+
+    public function deleteQuest(){
+        $this->db->where('id_quest', $this->input->post('id'));
+        $query = $this->db->delete('quesioner');
+      
+        $status = 500;
+
+        if($query){
+            $status = 200;
+        }
+
+        
+        $this->output
+            ->set_content_type('application/json')
+            ->set_output(json_encode($status));
+        
+    }
+
 }
--- application/views/templates/pages/PageQuest.php
+++ application/views/templates/pages/PageQuest.php
@@ -3,20 +3,21 @@
 </div>
 
 <!-- Tambah -->
-<button type="button" class="btn btn-primary mb-2 mt-3" data-bs-toggle="modal" data-bs-target="#exampleModal">
+<button type="button" class="btn btn-primary mb-2 mt-3" data-bs-toggle="modal" data-bs-target="#modalQuest" id="addModal">
   Tambah
 </button>
 
 <!-- Modal Add -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="modalQuest" tabindex="-1" aria-labelledby="modalQuestLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Add Quesioner</h1>
+        <h1 class="modal-title fs-5" id="modalQuestLabel">Add Quesioner</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form action="<?= base_url(); ?>QuestController/insert" method="POST">
+        <form method="POST" id="form_id">
+            <input type="hidden" name="id_quest" id="id_quest">
                 <div class="input-group">
                     <span class="input-group-text">Quesioner</span>
                     <textarea class="form-control" aria-label="With textarea" name="quest" id="quest"></textarea>
@@ -45,32 +46,32 @@
                 <div id="ganda" class="d-none mt-1">
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="">a</span>
-                        <input type="text" class="form-control" aria-label="ans_1" name="pl1" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" aria-label="ans_1" name="pl1"  id="pl1" aria-describedby="basic-addon1">
                     </div>
 
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="">b</span>
-                        <input type="text" class="form-control" aria-label="ans_2" name="pl2" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" aria-label="ans_2" name="pl2" id="pl2" aria-describedby="basic-addon1">
                     </div>
 
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="">a</span>
-                        <input type="text" class="form-control" aria-label="ans_3" name="pl3" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" aria-label="ans_3" name="pl3" id="pl3" aria-describedby="basic-addon1">
                     </div>
 
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="">c</span>
-                        <input type="text" class="form-control" aria-label="ans_4" name="pl4" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" aria-label="ans_4" name="pl4" id="pl4" aria-describedby="basic-addon1">
                     </div>
 
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="">e</span>
-                        <input type="text" class="form-control" aria-label="ans_5" name="pl5" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" aria-label="ans_5" name="pl5" id="pl5" aria-describedby="basic-addon1">
                     </div>
 
                     <div class="input-group mb-3">
                         <span class="input-group-text" id="">f</span>
-                        <input type="text" class="form-control" aria-label="ans_6   " name="pl6" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" aria-label="ans_6   " name="pl6" id="pl6" aria-describedby="basic-addon1">
                     </div>
 
                     <div class="d-flex gap-2">
@@ -78,13 +79,13 @@
                             Apakah ingin tambah pilihan "<b>Lainnya</b>" ?
                         </p>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="lainnya_pil" id="ya" value="1">
+                            <input class="form-check-input" type="radio" name="lainnya_pil" id="lainnya_ya" value="1">
                             <label class="form-check-label" for="ya">
                                 Ya
                             </label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="lainnya_pil" id="tidak" value="2">
+                            <input class="form-check-input" type="radio" name="lainnya_pil" id="lainnya_tidak" value="2">
                             <label class="form-check-label" for="tidak">
                                 Tidak
                             </label>
@@ -104,8 +105,32 @@
     </div>
   </div>
 </div>
-
 <!--  -->
+
+<!-- Small Modal -->
+<div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="deleteModalLabel">Delete Quest!</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+            <h6>
+                Anda Yakin ingin menghapus Quesioner?
+            </h6>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
+        <button type="button" class="btn btn-danger" id="btnDelete">Hapus</button>
+      </div>
+
+
+    </div>
+  </div>
+</div>
+<!-- Small Modal -->
+
 
 <table id="tableQuest" class="table table-striped" style="width:100%">
     <thead>
@@ -122,6 +147,9 @@
 </table>
 
 <script>
+
+    
+
     $(document).ready(function() {
 
         $('#tableQuest').DataTable({
@@ -148,5 +176,118 @@
     $('#text_bebas').on('click', function() {
         $('#ganda').addClass('d-none');
     });
+    
+    $('#addModal').on('click', function() {
+
+        $('#form_id').attr('action', '<?= base_url(); ?>QuestController/insert');
+
+        $('#modalQuestLabel').text('Add Quesioner');
+
+        $('#quest').text('');
+
+        if($('#pilihan_ganda').is(':checked')){
+            $('#pilihan_ganda').prop('checked', false);
+        }
+
+        if($('#text_bebas').is(':checked')){
+            $('#text_bebas').prop('checked', false);
+        }
+
+        if($('#lainnya_ya').is(':checked')){
+            $('#lainnya_ya').prop('checked', false);
+        }
+
+        if($('#lainnya_tidak').is(':checked')){
+            $('#lainnya_tidak').prop('checked', false);
+        }
+
+        $('#ganda').addClass('d-none');
+        $('#id_quest').val("");
+
+        for($i = 1; $i<=6; $i++){
+            $('#pl'+$i).val('');
+        }
+
+
+    });
+
+    function getDataQuest(id){
+        var dataToSend = { id : id};
+
+        // Show Modal
+        $('#modalQuest').modal('show');
+
+        // Change Title Modal
+        $('#modalQuestLabel').text('Update Quesioner');
+
+        $('#form_id').attr('action', '<?= base_url(); ?>QuestController/update');
+
+        // Get data
+        $.ajax({
+            type: "POST",
+            data: dataToSend,
+            url: "<?= base_url('QuestController/getEditQuest'); ?>",
+            success: function(response){
+                console.log(response);
+
+                let data = response.getDataQuest;
+
+                $('#quest').text(data.quest);
+                $('#id_quest').val(data.id_quest);
+
+                var temp = new Array();
+
+                temp = JSON.parse(data.ganda);
+
+                temp.forEach((element, index) => {
+                    $('#pl'+ (index +1)).val(element);
+                });
+
+                console.log(temp);
+
+                if(data.text_bebas == 0){
+                    $('#pilihan_ganda').click();
+                }else{
+                    $('#text_bebas').click();
+                }
+
+                if(data.lainnya == 1){
+                    $('#lainnya_ya').click();
+                }else{
+                    $('#lainnya_tidak').click();
+                }
+
+            }
+        })
+
+
+    }
+
+    function deleteQuest(id){
+        console.log(id);
+        var dataToSend = { id : id};
+
+        $('#deleteModal').modal('show');
+
+        $('#btnDelete').on('click', function() {
+            // alert("Hapus");
+            $.ajax({
+            type: "POST",
+            data: dataToSend,
+            url: "<?= base_url('QuestController/deleteQuest'); ?>",
+            success: function(response){
+                console.log(response);
+                if(response == 200){
+                    alert("Data Berhasil Dihapus!");
+
+                    location.reload(true);
+
+                }
+            }
+        })
+        });
+
+
+    }
 
 </script>
